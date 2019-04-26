bf7888ef93631052ba699bf31c894cff73734a31
Elijah Manor <elijah.manor@gmail.com>
2019-04-25 22:14:57 -0500

    Function with Custom Hook

--- KeyCode.js
+++ KeyCode.js
@@ -1,16 +1,19 @@
 import React, { Fragment, useState, useEffect } from "react";
 import KeyCodeBlock from "./KeyCodeBlock";
 
-export default function KeyCode() {
-  const [{ keyCode, key, code }, setCodes] = useState({});
-
+const useDocumentKeyCode = () => {
+  const [codes, setCodes] = useState({});
   useEffect(() => {
     const handleKey = ({ keyCode, key, code }) =>
       setCodes({ keyCode, key, code });
     document.addEventListener("keydown", handleKey);
     return () => document.removeEventListener("keydown", handleKey);
   }, []);
+  return codes;
+};
 
+export default function KeyCode() {
+  const { keyCode, key, code } = useDocumentKeyCode();
   return (
     <main className="KeyCode">
       {keyCode ? (
